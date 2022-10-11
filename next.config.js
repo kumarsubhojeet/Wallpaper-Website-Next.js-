ccc4d7aa17ba1d276b41a8c8508885e106c040d6
kumar- subhojeet <you@example.com>
2022-10-11 15:30:02 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    apiKey: 'Asf0i8szhvCa0P0ihwzRSDGSABZVn7PbHtfaxviZ_J0',
+  },
+}
+
+module.exports = nextConfig
