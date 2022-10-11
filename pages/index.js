af50f1447f1164243e0f39aa8f8fb87f73bd73f7
kumar- subhojeet <you@example.com>
2022-10-11 15:25:53 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Home from "./home/index";
 import Navbar from "./Navbar";
-import Category from './Category'
 
 const index = () => {
   return (
     <>
       <Navbar />
-      {/* <Category /> */}
       <Home />
     </>
   );
